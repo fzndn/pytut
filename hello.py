0307156a6bb4842d0d692fd8e31939f3b79ae015
Fauzan Deni Rywannis <fauzandeni176@gmail.com>
2020-09-20 11:42:21 +0700

    Create rumus_abc function

---
+++ hello.py
@@ -0,0 +1,12 @@
+import math
+
+def rumus_abc(a, b, c):
+    x_1 = (-b + math.sqrt(b**2 - 4 * a * c))/(2 * a)
+
+    x_2 = (-b - math.sqrt(math.pow(b, 2) - 4 * a * c))/(2 * a)
+
+    print(x_1)
+    print(x_2)
+
+rumus_abc(1, 2, 1)    # x_1 = -1.0     x_2 = -1.0
+rumus_abc(2, -5, -3)  # x_1 = -0.5     x_2 =  3.0
