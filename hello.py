0fe231abdf53618cd35e6f3b73d45091113b714c
Fauzan Deni Rywannis <fauzandeni176@gmail.com>
2020-09-20 12:58:08 +0700

    Beautify print

--- hello.py
+++ hello.py
@@ -5,8 +5,7 @@ def rumus_abc(a, b, c):
 
     x_2 = (-b - math.sqrt(math.pow(b, 2) - 4 * a * c))/(2 * a)
 
-    print(x_1)
-    print(x_2)
+    print("Akar-akarnya adalah " + str(x_1) + " dan " + str(x_2) + ".")
 
 rumus_abc(1, 2, 1)    # x_1 = -1.0     x_2 = -1.0
 rumus_abc(2, -5, -3)  # x_1 = -0.5     x_2 =  3.0
